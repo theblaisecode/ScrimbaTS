e4ddf8448e20eac119c7eb2f5959fa0112990fdc
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2024-12-21 08:24:45 +0100

    first commit

--- project/src/main.ts
+++ project/src/main.ts
@@ -1,1 +1,37 @@
 import "./style.css";
+
+// Number Types mini-challenge
+// Write a function that will only accept numbers and attend to
+// all TypeScript weakness flags.
+export const reviewTotalDisplay =
+  document.querySelector<HTMLSpanElement>("#reviews");
+
+const reviews = [
+  {
+    name: "Sheia",
+    stars: 5,
+    loyaltyUser: true,
+    date: "01-04-2021",
+  },
+  {
+    name: "Andrzej",
+    stars: 3,
+    loyaltyUser: false,
+    date: "28-03-2021",
+  },
+  {
+    name: "Omar",
+    stars: 4,
+    loyaltyUser: true,
+    date: "27-03-2021",
+  },
+];
+
+function onlyNum(reviewAmount: number) {
+  if (reviewTotalDisplay) {
+    reviewTotalDisplay.innerHTML = reviewAmount.toString();
+  }
+  return;
+}
+
+onlyNum(reviews.length);
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,2 @@
 import "./style.css";
 
-// Number Types mini-challenge
-// Write a function that will only accept numbers and attend to
-// all TypeScript weakness flags.
-const reviewTotalDisplay = document.querySelector("#reviews");
-
-
-const reviews = [
-  {
-    name: "Sheia",
-    stars: 5,
-    loyaltyUser: true,
-    date: "01-04-2021",
-  },
-  {
-    name: "Andrzej",
-    stars: 3,
-    loyaltyUser: false,
-    date: "28-03-2021",
-  },
-  {
-    name: "Omar",
-    stars: 4,
-    loyaltyUser: true,
-    date: "27-03-2021",
-  },
-];
