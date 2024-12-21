63c5522afed8107dbf81a5276c1cf7e854be96b0
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2024-12-21 09:49:24 +0100

    first commit

--- project/src/main.ts
+++ project/src/main.ts
@@ -3,8 +3,16 @@ import "./style.css";
 // Number Types mini-challenge
 // Write a function that will only accept numbers and attend to
 // all TypeScript weakness flags.
+
+// String Types mini-challenge
+// Write a function that will display the most recent reviewers name next to the review total,
+// making sure to assign a type to the parameter, to prevent unwanted behaviour.
+
 export const reviewTotalDisplay =
   document.querySelector<HTMLSpanElement>("#reviews");
+
+export const recentReview =
+  document.querySelector<HTMLHeadingElement>("#mostRecent");
 
 const reviews = [
   {
@@ -27,11 +35,18 @@ const reviews = [
   },
 ];
 
-function onlyNum(reviewAmount: number) {
+function onlyNum(reviewAmount: number, recent: string) {
   if (reviewTotalDisplay) {
     reviewTotalDisplay.innerHTML = `Number of reviews: ${reviewAmount.toString()}`;
   }
+
+  if (recentReview) {
+    recentReview.innerHTML = `Most recent review ${recent}`;
+  }
+  
   return;
 }
 
-onlyNum(reviews.length);
+onlyNum(reviews.length, reviews[reviews.length - 1].name);
+
+console.log(reviews.length - 1)
