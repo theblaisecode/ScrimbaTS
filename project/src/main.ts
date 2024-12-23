8abed579dbf4f042efe4595b68030843c102b2cc
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2024-12-23 08:51:42 +0100

    first commit

--- project/src/main.ts
+++ project/src/main.ts
@@ -7,6 +7,8 @@ export const reviewTotalDisplay =
 export const recentReview =
   document.querySelector<HTMLHeadingElement>("#mostRecent");
 
+const allProperties = document.querySelector<HTMLDivElement>(".properties");
+
 let isOpen: boolean;
 
 const reviews: {
@@ -103,3 +105,14 @@ onlyNum(
 );
 
 populateUser(you.isReturning, you.firstName);
+
+//Add the properties
+for (let i = 0; i < properties.length; i++) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = properties[i].title;
+  const image = document.createElement("img");
+  image.setAttribute("src", properties[i].image);
+  card.appendChild(image);
+  allProperties.appendChild(card);
+}
